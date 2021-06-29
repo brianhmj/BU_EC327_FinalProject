229b1e7313419cf6f332d54649d9a37535f00e0c
pdvnny <parker_dunn@outlook.com>
2021-06-29 16:36:08 -0400

    created main.cpp
    Ben and I tried to plan out what to include in the main script that controlled out application. There are notes toward the top of the script that note a few ides.
    
    Ben and I also made some separate notes in other places about how we thought we should structure the overall project that we can consolidate somewhere later today.

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// Copyright 2021 Parker Dunn pgdunn@bu.edu
+// Copyright 2021 
+// Copyright 2021
+// Copyright 2021
+
+
+
+
+// Includes features in this script:
+
+/*
+-- while loop controlling the application functions
+     - display 7-day window
+     - display 1-day window
+     - display all appointments saved
+     - allow entry of appointment information via the terminal (retreive from cin)
+
+-- keep track of time in the while loop
+
+-- if appoint is entered -> format it -> save to file (JSON)
+      - month
+      - day
+      - year
+      - appointment name
+      - military time
+
+
+*/
+
+int main() {
+
+
+
+
+  return 0;
+}
